a630bf8eda893757c5add53ff2b49a04e3860b31
Piquin-Dylan <dylan.piquin@student.hepl.be>
2025-04-14 15:39:48 +0200

    Form + responsive

--- form.php
+++ form.php
@@ -5,38 +5,41 @@
 get_header();
 ?>
 
+<div class="container">
+    <form action="#" method="post">
+        <section class="container_form">
+            <h2 class="title_form">Contactez-moi </h2>
+            <label for="name">Nom complet</label>
+            <input class="input_form" type="text" name="name" id="name" placeholder="Ex: Jean Dupont" required>
+            <label for="email">Adresse mail</label>
+            <input class="input_form" type="email" name="email" id="email" placeholder=" Ex: jeandupont@example.com"
+                   required>
+            <label for="tel">Numéros de téléphone</label>
+            <input class="input_form" type="tel" name="tel" id="tel" placeholder="+ 32 451 20 67 90 " required>
+            <label for="subject">Sujet</label>
+            <input class="input_form" type="text" id="subject" name="subject" placeholder="Ex: Sujet du mail" required>
+            <label for="area">Votre message</label>
+            <textarea name="area" id="area" cols="30" rows="10" placeholder="Ex: Entre votre message"
+                      required></textarea>
 
-<form action="#" method="post">
-    <section class="container_form">
-        <h2 class="title_form">Contactez-moi </h2>
-        <label for="name">Nom complet</label>
-        <input class="input_form" type="text" name="name" id="name" placeholder="Ex: Jean Dupont" required>
-        <label for="email">Adresse mail</label>
-        <input class="input_form" type="email" name="email" id="email" placeholder=" Ex: jeandupont@example.com"
-               required>
-        <label for="tel">Numéros de téléphone</label>
-        <input class="input_form" type="tel" name="tel" id="tel" placeholder="+ 32 451 20 67 90 " required>
-        <label for="subject">Sujet</label>
-        <input class="input_form" type="text" id="subject" name="subject" placeholder="Ex: Sujet du mail" required>
-        <label for="area">Votre message</label>
-        <textarea name="area" id="area" cols="30" rows="10" placeholder="Ex: Entre votre message" required></textarea>
-    </section>
-</form>
-<aside>
-    <h2 class="title_coordonnées">Mes coordonnées</h2>
-    <ul>
-        <li>
-            Numéros de téléphone
-            <a href="#"><?php the_field('num_tel') ?></a>
-        </li>
-        <li>Adresse mail
-            <a href="#"><?php the_field('adresse_mail') ?></a>
-        </li>
-        <li>Adresse Postale
-            <a href="#"><?php the_field('adresse') ?></a>
-        </li>
-    </ul>
-</aside>
-
+            <input type="button" value="Envoyer">
+        </section>
+    </form>
+    <aside>
+        <h2 class="title_coordonnées">Mes coordonnées</h2>
+        <ul class="liste_cord">
+            <li>
+                Numéros de téléphone
+                <a class="item" href="#"><?php the_field('num_tel') ?></a>
+            </li>
+            <li>Adresse mail
+                <a class="item" href="#"><?php the_field('adresse_mail') ?></a>
+            </li>
+            <li>Adresse Postale
+                <a class="item" href="#"><?php the_field('adresse') ?></a>
+            </li>
+        </ul>
+    </aside>
+</div>
 
 <?php get_footer(); ?>
