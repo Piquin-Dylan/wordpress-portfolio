13be9bb48e9c0e09a7373d0df7819595836966a4
Piquin-Dylan <dylan.piquin@student.hepl.be>
2025-06-01 23:14:22 +0200

    fix bug form 7

--- functions.php
+++ functions.php
@@ -1,9 +1,10 @@
 <?php
-add_action('init', function() {
-    if (!session_id()) {
-        session_start();
-    }
-});
+// Démarrer le système de sessions pour pouvoir afficher des messages de feedback venant des formulaires.
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+require_once(__DIR__.'/forms/ContactForm.php');
 
 register_post_type('contact_message', [
     'label' => 'Messages de contact',
@@ -21,7 +22,6 @@ add_action('admin_post_dw_submit_contact_form', 'dw_handle_contact_form');
 add_action('admin_post_nopriv_dw_submit_contact_form', 'dw_handle_contact_form');
 
 // Chargement de notre class qui va gérer ce formulaire
-require_once(__DIR__.'/forms/ContactForm.php');
 function dw_handle_contact_form()
 {
     $form = (new \DW_Theme\Forms\ContactForm())
